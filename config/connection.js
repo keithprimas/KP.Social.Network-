c92b9154d7f154bbb5c452639edb0eb7ead785a5
keithprimas <136135449+keithprimas@users.noreply.github.com>
2023-12-10 19:25:54 -0500

    added model routes and connection.js

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb+srv://keithprimas:<n1fyovZzJcAOGFU0>@kpsocnet.k66nypb.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
+

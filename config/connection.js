b76c04df8ce3193d3fbf51537a1219d4f3d7f952
keithprimas <136135449+keithprimas@users.noreply.github.com>
2023-12-10 19:45:01 -0500

    added api routes

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb+srv://keithprimas:<n1fyovZzJcAOGFU0>@kpsocnet.k66nypb.mongodb.net/?retryWrites=true&w=majority",
+  process.env.MONGODB_URI || "mongodb+srv://keithprimas:n1fyovZzJcAOGFU0@kpsocnet.k66nypb.mongodb.net/?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
